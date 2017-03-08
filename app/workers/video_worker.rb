e78d9adfc98225e9cf7dae8180baa3bd1fd55234
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2017-03-08 23:35:51 +0300

    Added video player

--- app/workers/video_worker.rb
+++ app/workers/video_worker.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 class VideoWorker
   include Sidekiq::Worker
 
@@ -8,7 +10,10 @@ class VideoWorker
 
     watermark = video.watermark
 
-    output_file = 'processed_videos/' + random_file_name('mp4')
+    FileUtils.mkdir_p 'public/videos'
+    FileUtils.mkdir_p 'public/videos/processed_videos'
+
+    output_file = 'public/videos/processed_videos/' + random_file_name('mp4')
 
     ffmpeg_command_watermark = %{ 
       ffmpeg -i #{input_file} \
@@ -21,7 +26,7 @@ class VideoWorker
 
     system(ffmpeg_command_watermark)
 
-    video.processed_video = output_file
+    video.processed_video = output_file.gsub('public/videos/','')
     video.file_size = '%.2f' % (File.size(output_file).to_f / 2**20).to_s
     duration = (`#{ffprobe_command_duration}`.to_f / 60)
     video.duration = '%.2f' % duration.to_s
@@ -32,7 +37,7 @@ class VideoWorker
     while(screenshot_time < duration * 60) do 
       file_path = 'public/images/screenshots_videos/' + random_file_name('jpg')
       time = Time.at(screenshot_time.to_i).utc.strftime("%H:%M:%S")
-      ffmpeg_screenshot_command = "ffmpeg -ss #{time} -i #{input_file} -vframes 1 #{file_path}"
+      ffmpeg_screenshot_command = "ffmpeg -ss #{time} -i #{output_file} -vframes 1 #{file_path}"
       system(ffmpeg_screenshot_command)
       video.screenshots.create(file_path: file_path.gsub('public/images/',''))
       screenshot_time += screenshot_time
